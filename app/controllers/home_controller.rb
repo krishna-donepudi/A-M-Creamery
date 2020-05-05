8650f1341e9edfaffe45a283ed931326e35579aa
Krishna Donepudi <kdonepud@andrew.cmu.edu>
2020-05-05 00:58:43 -0400

    employee dashboard

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,13 +1,8 @@
 class HomeController < ApplicationController
   def index
     unless current_user.nil?
-      if current_user.role?(:employee)
-        redirect_to employee_path(current_user)
-      else
-        redirect_to dashboard_path
-      end
+      redirect_to dashboard_path
     end
-
   end
 
   def about
@@ -25,6 +20,12 @@ class HomeController < ApplicationController
       @employees = Employee.active.regulars.alphabetical.select{|e| e.current_assignment.nil? ? false : e.current_assignment.store == current_user.current_assignment.store}
       @shifts = Shift.for_store(current_user.current_assignment.store).finished.select{ |s| !s.report_completed? }
     end
+    dates = DateRange.new(Date.today.to_date, 7.days.ago.to_date)
+    payrollcalc = PayrollCalculator.new(dates)
+    @payroll = payrollcalc.create_payroll_record_for(current_user)
+    @upcoming_shifts = current_user.shifts.for_next_days(8)
+    @pending_shifts_today = current_user.shifts.pending.for_date(Date.current)
+    @started_shifts_today = current_user.shifts.started.for_date(Date.current)
   end
 
   def search
